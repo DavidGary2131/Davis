9e342d5acd8687d7d3ba9e877f17b17af4b126d0
david <david@David-PC>
2020-07-10 19:14:05 -0500

    Listo para la nube

---
+++ src/main/java/com/unitec/Androide/AndroideApplication.java
@@ -0,0 +1,13 @@
+package com.unitec.Androide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AndroideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AndroideApplication.class, args);
+	}
+
+}
